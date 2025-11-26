34bb61c24e151fc36b27d1847675cf42ce01680a
Isleño <0322104078@ut-tijuana.edu.mx>
2025-11-26 09:24:16 -0800

    Prodcution
    nasheee deahhh

--- Viajes/run_production.bat
+++ Viajes/run_production.bat
@@ -2,4 +2,4 @@
 REM Script para correr Django en modo PRODUCCION
 echo Corriendo Django en modo PRODUCCION...
 set DJANGO_SETTINGS_MODULE=Viajes.settings.production
-python manage.py runserver 0.0.0.0:8001
+python manage.py runserver 0.0.0.0:8002
